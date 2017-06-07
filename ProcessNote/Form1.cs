0c0e4378d22548c6e048f24d41407212abac01f9
Noteee <lugosi.norbert@gmail.com>
2017-06-07 11:21:18 +0200

    ProcessNote

--- ProcessNote/Form1.cs
+++ ProcessNote/Form1.cs
@@ -7,14 +7,92 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
 
 namespace ProcessNote
 {
     public partial class Form1 : Form
     {
+        private Dictionary<string, string> comments = new Dictionary<string, string>();
+
         public Form1()
         {
             InitializeComponent();
+            listProcesses();
+            
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.TopMost = true;
+        }
+
+        private void listProcesses()
+        {
+            Process[] processlist = Process.GetProcesses();
+            foreach (var process in processlist)
+            {
+                listBox1.Items.Add(process.ProcessName);
+            }
+            textBox1.Text = listBox1.Items.Count.ToString() + " - Processes Runnning";
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            listProcesses();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string selectedProcess = listBox1.SelectedItem.ToString();
+                using (PerformanceCounter pcProcess =
+                    new PerformanceCounter("Process", "% Processor Time", selectedProcess))
+                {
+                    pcProcess.NextValue();
+                    System.Threading.Thread.Sleep(500);
+                    int memsize = 0; // memsize in Megabyte
+                    PerformanceCounter PC = new PerformanceCounter();
+                    PC.CategoryName = "Process";
+                    PC.CounterName = "Working Set - Private";
+                    PC.InstanceName = selectedProcess;
+                    memsize = Convert.ToInt32(PC.NextValue()) / (int) (1024);
+                    PC.Close();
+                    PC.Dispose();
+                    textBox3.Text = ("CPU--->" + (Math.Round(pcProcess.NextValue(), 1) + "%") + "\r\n");
+                    textBox3.Text += "Memory--->" + memsize + "KB";
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+            }
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            string curItem = listBox1.SelectedItem.ToString();
+            textBox4.Text = curItem + "|\r\n";
+            foreach(var item in comments)
+            {
+                if (item.Key.Equals(listBox1.SelectedItem.ToString()))
+                {
+                    textBox4.Text += item.Value;
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var tokens = textBox4.Text.Split('|');
+            System.Console.WriteLine(tokens[0]);
+            System.Console.WriteLine(tokens[1]);
+           comments.Add(tokens[0],tokens[1]);
+        }
+
     }
 }
