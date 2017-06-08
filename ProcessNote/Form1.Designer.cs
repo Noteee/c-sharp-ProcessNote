7d188458639e9e865bff8a1b4aa8af2f96dfe775
Noteee <lugosi.norbert@gmail.com>
2017-06-08 11:51:43 +0200

    on/off top feature

--- ProcessNote/Form1.Designer.cs
+++ ProcessNote/Form1.Designer.cs
@@ -39,6 +39,7 @@ namespace ProcessNote
             this.webBrowser1 = new System.Windows.Forms.WebBrowser();
             this.textBox4 = new System.Windows.Forms.TextBox();
             this.button3 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // button1
@@ -112,7 +113,7 @@ namespace ProcessNote
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(631, 156);
+            this.button3.Location = new System.Drawing.Point(590, 160);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(80, 62);
             this.button3.TabIndex = 8;
@@ -120,11 +121,23 @@ namespace ProcessNote
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(696, 184);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(59, 17);
+            this.checkBox1.TabIndex = 9;
+            this.checkBox1.Text = "OnTop";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(767, 749);
+            this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.textBox4);
             this.Controls.Add(this.webBrowser1);
@@ -155,6 +168,7 @@ namespace ProcessNote
         private System.Windows.Forms.WebBrowser webBrowser1;
         private System.Windows.Forms.TextBox textBox4;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.CheckBox checkBox1;
     }
 }
 
--- ProcessNote/Form1.cs
+++ ProcessNote/Form1.cs
@@ -8,12 +8,16 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Diagnostics;
+using System.Runtime.CompilerServices;
 
 namespace ProcessNote
 {
     public partial class Form1 : Form
     {
         private Dictionary<string, string> comments = new Dictionary<string, string>();
+        
+        
+        
 
         public Form1()
         {
@@ -24,7 +28,7 @@ namespace ProcessNote
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            this.TopMost = true;
+            this.TopMost = false;
         }
 
         private void listProcesses()
@@ -32,7 +36,7 @@ namespace ProcessNote
             Process[] processlist = Process.GetProcesses();
             foreach (var process in processlist)
             {
-                listBox1.Items.Add(process.ProcessName);
+                listBox1.Items.Add(process.ProcessName + " " + process.Id.ToString());
             }
             textBox1.Text = listBox1.Items.Count.ToString() + " - Processes Runnning";
 
@@ -48,7 +52,8 @@ namespace ProcessNote
         {
             try
             {
-                string selectedProcess = listBox1.SelectedItem.ToString();
+                var tokens = listBox1.SelectedItem.ToString().Split(' ');
+                string selectedProcess = tokens[0];
                 using (PerformanceCounter pcProcess =
                     new PerformanceCounter("Process", "% Processor Time", selectedProcess))
                 {
@@ -94,5 +99,9 @@ namespace ProcessNote
            comments.Add(tokens[0],tokens[1]);
         }
 
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            this.TopMost = !this.TopMost;
+        }
     }
 }
